3fc0636b0df61da31d7ee52c9ad35e6beb08d62b
DRodriguez2 <diego.rodriguez@hcl.com>
2021-02-25 09:27:26 -0500

    small syntactical update

--- src/main/java/functions/AddFile.java
+++ src/main/java/functions/AddFile.java
@@ -7,11 +7,11 @@ import utilities.Utility;
 public class AddFile extends Utility {
 
 	protected void add(String _fileName, String _directory) {
-		File file = new File(_directory + "\\" + _fileName);
+		File file = new File(_directory + "\\" + _fileName); //for Windows machines
 		try {
 			
 			if(file.createNewFile()) System.out.println("File creation successful\n");
-			else System.out.println("Unable to create file, please make sure file name is unique.");
+			else System.out.println("Unable to create file, please make sure file name is unique and adheres with file naming conventions of your OS.");
 			
 		} catch (IOException e) {
 			Logger.error("Error creating file");
--- src/main/java/functions/DeleteFile.java
+++ src/main/java/functions/DeleteFile.java
@@ -4,7 +4,7 @@ import java.io.File;
 public class DeleteFile {
 
 	protected void delete(String _fileName, String _directory) {
-		File file = new File(_directory + "\\" + _fileName);
+		File file = new File(_directory + "\\" + _fileName); // for windows machines
 
 		if(file.delete()) System.out.println("File deletion successful\n");
 		else System.out.println("Unable to delete file, please make sure file name is correct");
